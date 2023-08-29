b159283b8d15760265082d487f001b04083a502f
jaupi98 <121817204+jaupi98@users.noreply.github.com>
2023-08-29 17:07:16 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+         <h1 class="text-center">auth</h1>
+        </div>
+        <div class="col-12 mt-4">
+            <div class="d-flex justify-content-center">
+                <a href="#" class="btn btn-primary mx-2"> Visualizza file</a>
+                <a href="#" class="btn btn-primary mx-2"> Aggiungi file</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
