99e4c9776c92a5962a00e24c7c9ec8e948225f1c
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-08-30 00:59:02 +0200

    seader

--- app/Models/project.php
+++ app/Models/project.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Type;
+
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'date_of_creation', 'cover_image'];
+    protected $fillable = ['type_id', 'title', 'description', 'date_of_creation', 'cover_image'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/type.php
+++ app/Models/type.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
-class type extends Model
+use App\Models\Project;
+
+class Type extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
+
+    public static function generateSlug($name){
+        return Str::slug($name, '-');
+    }
 }
